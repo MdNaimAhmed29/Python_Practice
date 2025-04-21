8127aade8ab264f5aa93140dc5d8db3dfd0836a1
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-21 22:10:51 +0600

    submit

--- First-Code/main.py
+++ First-Code/main.py
@@ -2,12 +2,15 @@ print("Hello World")
 
 x = 1000
 y = 2000
-
+# single line comments
 sum = x + y
 sub = x - y
 mul = x * y
 div = x / y
 
+"""
+multiline comments
+"""
 print(x)
 print(y)
 print(sum)
