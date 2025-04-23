6956b2f5a3e15a3899aa5a8cd6090246f23b029d
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-23 19:10:59 +0600

    updated

--- String_Practice/Number.py
+++ String_Practice/Number.py
@@ -1,6 +1,36 @@
+import math
+
 x = 10
 y = 20
 z = 30
 
 results = x + y * z / x // y % z - x ** y
 print(results)
+
+result = math.sqrt(640)
+print(result)
+
+result2 = math.pow(64, 2)
+print(result2)
+
+result3 = math.sin(math.radians(66))
+print(result3)
+
+result4 = math.log(10)
+print(round(result4, 4))
+
+result5 = math.log10(10)
+print(round(result5, 4))
+
+result6 = math.factorial(4)
+print(result6)
+
+result7 = math.gcd(6,12,80)
+print(result7)
+
+result8 = math.lcm(6,12,80)
+print(result8)
+
+result9 = math.pi
+print(round(result9, 4))
+
