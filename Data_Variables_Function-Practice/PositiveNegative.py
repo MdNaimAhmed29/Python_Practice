21df4bbf7e61a582aa55562a33605c6ecd0701c1
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-23 23:12:06 +0600

    Updated

--- Data_Variables_Function-Practice/PositiveNegative.py
+++ Data_Variables_Function-Practice/PositiveNegative.py
@@ -2,7 +2,28 @@ num = input("Enter a number: ")
 num = int(num)
 
 if num >= 0:
-    print(num, "is Positive")
+    if num % 2 == 0:
+        print(num, "is Positive and is an even number.")
+    else:
+        print(num, "is Negative and is an Odd number.")
 
 else:
-    print(num, "is Negative")
+    if num % 2 == 0:
+        print(num, "is Negative and is an even number.")
+    else:
+        print(num, "is Negative and is an Odd number.")
+
+#Two terms
+
+if num >= 0 and num % 2 == 0:
+    print(num, "is Positive and is an even number.")
+
+elif num >= 0 and num % 2 !=0:
+    print(num, "is Positive and is an odd number.")
+
+elif num < 0 and num % 2 == 0:
+    print(num, "is Negative and is an even number.")
+
+else:
+    print(num, "is Negative and is an Odd number.")
+
