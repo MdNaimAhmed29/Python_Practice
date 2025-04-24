c16f8fe34ff042b63c66ad7dfab587b244803c59
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-24 11:00:47 +0600

    updated

--- List/main.py
+++ List/main.py
@@ -1,1 +1,24 @@
+country = ["Bangladesh", "USA", "UK", "UAE"]
+print(type(country))
+print(country)
+print(country[0])
+print(country[1])
+print(country[2])
+print(country[3])
+print(country[-1])
+print(country[-2])
+print(country[-3])
+print(country[-4])
 
+country.append("Canada")
+print(country)
+
+print(len(country))
+
+print("USA" in country)
+print("Germany" in country)
+
+country.pop()
+print(country)
+
+ 
