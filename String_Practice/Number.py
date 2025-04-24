1f4a8d497d6a0a15e2177dda75cc6825578562e7
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-25 03:37:25 +0600

    updated

--- String_Practice/Number.py
+++ String_Practice/Number.py
@@ -1,36 +1,106 @@
+#Number
+"""
+Operators Order
+- (**) = Exponentiation
+- (+x, -y, ~z) = Unary plus, Unary Minus, Bitwise not
+- (*, /, //(floor division), %, +, -) = General Operators
+- (<< , >>) = Bitwise shift
+- and(&), or(|), not(!), xor(^) = Bitwise Operators
+- ==, !=, >, <, >=, <= = Relational Operators
+- is, is not = Identity Operators
+- in, not in = Membership Operators
+- and, or, not = Logical Operators
+----------------------------------------------------------------
+Most Uses Operators
++x, -y
+*, /, //(floor division), %, +, -
+and(&), or(|)
+==, !=, >, <, >=, <=
+
+Conversion
+- int -> float
+- float -> int
+- complex
+"""
+
 import math
 
 x = 10
 y = 20
 z = 30
+a = 3.1416
+b = 10+7j
+d = -50.45
 
-results = x + y * z / x // y % z - x ** y
-print(results)
+result10 = ("Addition ",x + y)
+result11 = ("Subtraction ",x - y)
+result12 = ("Multiplication ",x * y)
+result13 = ("Division ",x / y)
+result14 = ("Modulus ",x % y)
+result15 = ("Exponentiation ",x ** y)
+result16 = ("Floor division ",x // y)
+result17 = ("Remainder ",x % y)
+result18 = ("Bits ",x % y)
+result19 = ("Square ",math.sqrt(x))
+result20 = ("Power", math.pow(x, y))
+result21 = ("Sin ",math.sin(math.radians(x+y)))
+result22 = ("log ",math.log(x))
+result23 = ("log10 ",math.log10(x))
+result24 = ("factorial ",math.factorial(x))
+result25 = ("GCD ",math.gcd(x,y,z))
+result26 = ("LCM ",math.lcm(x,y,z))
+result27 = math.pi
+result28 = ("Constant ", math.e)
+result29 = ("Absulate Value ",math.fabs(d))
+result30 = ("floor(Positive Value) ",math.floor(a)) #Rounds down to the nearest integer
+result31 = ("floor(Negative Value) ",math.floor(d)) #rounds toward negative
+result32 = ("Ceiling(Positive Value) ",math.ceil(a)) #Rounds up to the nearest integer
+result33 = ("Ceiling(Negative Value) ",math.ceil(d)) #rounds toward positive
 
-result = math.sqrt(640)
-print(result)
+print(result10)
+print(result11)
+print(result12)
+print(result13)
+print(result14)
+print(result15)
+print(result16)
+print(result17)
+print(result18)
+print(result19)
+print(result20)
+print(result21)
+print(result22)
+print(result23)
+print(result24)
+print(result25)
+print(result26)
+print("pi ",round(result27, 4))
+print(result28)
+print(result29)
+print(result30)
+print(result31)
+print(result32)
+print(result33)
 
-result2 = math.pow(64, 2)
-print(result2)
+print("")
 
-result3 = math.sin(math.radians(66))
-print(result3)
+print("Int to Float ", float(x), type(float(x)))
+print("Float to Int ", int(a), type(int(a)))
+print("Int to Complex ", complex(x), type(complex(x)))
+print("Float to Complex ", complex(a), type(complex(a)))
+print("Int to String ", str(x), type(str(x)))
+print("float to String ", str(a), type(str(a)))
+print("Complex to String ", str(b), type(str(b)))
 
-result4 = math.log(10)
-print(round(result4, 4))
+print("")
 
-result5 = math.log10(10)
-print(round(result5, 4))
+try:
+    print("Complex to int ", int(b), type(int(b)))
+    print("Complex to float ", float(b), type(float(b)))
+except Exception as error:
+    print("Error 1: ", error)
 
-result6 = math.factorial(4)
-print(result6)
-
-result7 = math.gcd(6,12,80)
-print(result7)
-
-result8 = math.lcm(6,12,80)
-print(result8)
-
-result9 = math.pi
-print(round(result9, 4))
-
+try:
+    print("Complex to float ", float(b), type(float(b)))
+except Exception as error:
+    print("Error 2: ", error)
