6e4770d3aae76fbd5059035aeb7987e740ec70b9
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-24 14:01:18 +0600

    update

--- Forloop/main.py
+++ Forloop/main.py
@@ -17,7 +17,6 @@ for i in range(0, 10, 2):
     print(i)
 
 #use loop
-
 def generate_multiplication_table(n):
     print(n, "x 1 =", n * 1)
     print(n, "x 2 =", n * 2)
@@ -36,3 +35,12 @@ n = int(n)
 for i in range(1, n+1):
     generate_multiplication_table(i)
     print("")
+
+#for loop use two
+def multplication_table(n):
+    for i in range(1, 11):
+        print("{} x {} = {}".format(n, i, i * n))
+
+n = input("Enter n: ")
+n = int(n)
+multplication_table(n)
