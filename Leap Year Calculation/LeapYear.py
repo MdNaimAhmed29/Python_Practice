84cfb5b122d3107acbaf57b632bfb9a6e8f531e3
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-24 10:38:09 +0600

    updated

--- Leap Year Calculation/LeapYear.py
+++ Leap Year Calculation/LeapYear.py
@@ -1,4 +1,16 @@
 year = input("Enter the year: ")
 year = int(year)
 
+if year % 4 != 0:
+   print(year, "is not a leap year")
+
+elif year % 100 != 0:
+    print(year, "is a leap year")
+
+elif year % 400 != 0:
+    print(year, "is not a leap year")
+
+else:
+    print(year, "is a leap year")
+
 
