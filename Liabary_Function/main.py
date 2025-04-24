874bc3985492064fb8db58a1be9d277782c594d9
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-24 13:17:14 +0600

    Added

--- Liabary_Function/main.py
+++ Liabary_Function/main.py
@@ -1,1 +1,12 @@
+import random
+import datetime
+import math
 
+random_number = random.randint(1, 100)
+print(random_number)
+
+today = datetime.datetime.today()
+print(today)
+
+x = math.sqrt(random_number)
+print(x)
