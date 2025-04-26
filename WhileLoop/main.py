cc55eb7cdf9ca69d244ea21f87729bba8563b484
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-26 21:45:48 +0600

    updated

--- WhileLoop/main.py
+++ WhileLoop/main.py
@@ -1,3 +1,6 @@
+#need start and end point and increment
+from operator import index
+
 i = 1
 
 while i <= 10:
@@ -79,3 +82,40 @@ while num3 < 7:
         print("num3: ", num3, "num4: ", num4, "total", num3 + num4)
         num4 += 1
     num3 += 1
+
+
+fruits = ['Apple', 'Banana', 'Mango']
+
+index = 0
+while index < len(fruits):
+    print(fruits[index])
+    index += 1
+
+index = 0
+end = 10
+while index < end:
+    if index == 5:
+        break
+    print(index)
+    index += 1
+
+print("")
+
+ind = 0
+end = 10
+while ind < end:
+    if ind == 5:
+        ind += 1 #increment index to avoid an infinite loop
+        continue
+    print(ind)
+    ind += 1
+
+examresults = {'phy':90, 'math':89, 'bangla':70} #dictonary
+keys = list(examresults.keys())
+
+ind = 0
+while ind < len(keys):
+    eachkey = keys[ind]
+    print(examresults[eachkey])
+    ind += 1
+
