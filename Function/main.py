aae8bebae47ee45d3f9dd5eb59b78999ddd2e3c9
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-29 11:33:32 +0600

    updated

--- Function/main.py
+++ Function/main.py
@@ -1,18 +1,61 @@
-def generate_multiplication_table(num):
-    print(num, "x 1 =", num * 1)
-    print(num, "x 2 =", num * 2)
-    print(num, "x 3 =", num * 3)
-    print(num, "x 4 =", num * 4)
-    print(num, "x 5 =", num * 5)
-    print(num, "x 6 =", num * 6)
-    print(num, "x 7 =", num * 7)
-    print(num, "x 8 =", num * 8)
-    print(num, "x 9 =", num * 9)
-    print(num, "x 10 =", num * 10)
+def generate_multiplication_table(numb):
+    print(numb, "x 1 =", numb * 1)
+    print(numb, "x 2 =", numb * 2)
+    print(numb, "x 3 =", numb * 3)
+    print(numb, "x 4 =", numb * 4)
+    print(numb, "x 5 =", numb * 5)
+    print(numb, "x 6 =", numb * 6)
+    print(numb, "x 7 =", numb * 7)
+    print(numb, "x 8 =", numb * 8)
+    print(numb, "x 9 =", numb * 9)
+    print(numb, "x 10 =", numb * 10)
 
-num = input("Enter a number: ")
-num = int(num)
-generate_multiplication_table(num)
-generate_multiplication_table(num+1)
+numb = input("Enter a number: ")
+numb = int(numb)
+generate_multiplication_table(numb)
+generate_multiplication_table(numb+1)
+
+numbers = [1, 2, 3, 4, 5]
+max_number = max(numbers)
+
+print(max_number)
+
+#linear search
+
+max_num = float('-inf')
+for n in numbers:
+    if n > max_num:
+        max_num = n
+print(max_num)
+
+import random
+
+print(dir(random))
+print(help(random.randint))
+num = []
+for _ in range(10):
+    num.append(random.randint(1, 100))
+    print(num)
+
+
+def my_function(a, b, c): #paramitar
+    a = 5 #local variable
+    b = 6
+    c = 7
+    e = 9
+    global d
+    d= d*2
+    print("a = ", a)
+    print("b = ", b)
+    print("c = ", c)
+    print("e = ", e)
+    print("d = ", d)
+    n = a + b + c
+
+    return n
+
+d = 8 #global variable
+x = my_function(1, 2, 3) #arguments
+print(x)
 
 
