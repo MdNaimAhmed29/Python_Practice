4d6ce6d0416c11ee3e4630898ceb8433ba090883
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-30 10:49:59 +0600

    updated

--- Function/main.py
+++ Function/main.py
@@ -3,6 +3,7 @@ required parameters ->(a,b) always need arguments
 default parameters ->(a=100,b) no need arguments for a
 variable length arguments ->(*numbers)/tuples-> multiple arguments
 keyword arguments -> (**person)/Dictionary -> multiple key and values
+small, anonymous, inline-> lamda function
 """
 
 
@@ -88,4 +89,17 @@ dictonary_new(
     city = "Chicago"
 )
 
+print("")
 
+result = lambda : 2+3
+print(result())
+
+print("")
+
+result2 = lambda x, y: x+y
+print(result2(10,50))
+
+print("")
+
+result3 = lambda x, y: x if x > y else y
+print(result3(10,50))
